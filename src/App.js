b1d7e8cef93a70b87d590873079a0307bbecb208
USER <USER@DESKTOP-GAP4S8J>
2024-09-09 17:01:24 +0530

    dffdfd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./styles.css";
+// import Products from "./Products"; //1 dot for one folder out
+import Products from "./Products/Products";//2 dot for two folder out
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Products />
     </div>
   );
 }
 
-export default App;
+// What is react
+// Why react
+// how react came into picture
+// Advantages of react
+
+// Usecases of react
+// How to use react
+// concepts of react
+// Cons of react
+
+// React
+// You can create components
+// In react everything is a component
+// and you can share components
+// Everything is going to be unidirectional
+// the flow would be from parent to child
+// React provides you with something known as Virtual dom
+// React is a frontend library whose job is to provide you with reusable components
+// which can be used as building blocks for a frontend application
+
+// How can I start with a react application
+
+// Node -Package- Library
+// NPM -Node package manager - Librarian
+// npm install react - Book card
+// npm
+// npx node pacakge execute
+
+// Check for node in your system
+// Check for NPM in your system
+// use npx create-react-app nameofapp
+
+// major.minor.patch
+
+// 12.1.2 current
+
+// 12.1.3 = patch
+
+// 12.2.2 minor
+
+// 13.0.0 Major
+
+// ^12.1.2
+
+// npm install
+// 12.9.9
+//13.0.0
+
+// minor and patch update would happen automatically
+// but major update can never ever happen automatically
+// bundelling of all the js's
+
+//  bundle.js which is going to be show to the end user
+// dev dependencies are not going to be added into that
+// webpack
+// bundle all the js files that are there into one js file which would be server to the end user
+
+// webpack is a bundler
+//************************************************************************
+//************************************************************************
+// react component
+//  react component is a function
+//  jsx
+// jsx is javascript xml notation
+// html written inside js is known as jsx
+
+// react component is a function which returns JSX
+// it should always start with Capital letter
+
+// document.createElement
+// document.appendchild
+
+//************************************************************************
+//************************************************************************
+
+// Named exports
+// Default exports
+// JS
+// You can export anything from a file
+// by writing export in front of the thing that you are looking to export
+// export var x =10;
+// export function abc() {}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />); //its just like  document.appendChild(a); we can say to understand:-
+// root.render(
+//   <div className="App">
+//     <h1>Hello CodeSandbox</h1>
+//     <h2>Start editing to see some magic happen!</h2>
+//   </div>
+// );  // it means here html returning inside java script
+
+// for your react app to be up and running you need
+// one root element
+// in the root element the whole react app would be mounted
+
+// var a = document.createElement("div")
+// document.appendChild(a);
+
+// react says that I need one root element
+// I will manipulate all the dom
+// I will create all the html elements and append it to the html
+
+// react is a generic library which will be used in order to create components and create html
+// react dom is what would be used for your interaction of react with the DOM
+// react native
+// react native interact with your mobile app,instead of   "react-dom": "^18.0.0" use react native for mobile applications.
+
+// abc(); normal call to the function
+// <Abc /> react call the components/functions like this,its jsx syntax
