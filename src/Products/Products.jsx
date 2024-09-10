348de5bec5abb0a19b3a18b54e52627f33697dac
USER <USER@DESKTOP-GAP4S8J>
2024-09-10 18:37:00 +0530

    ffgfgfg

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,17 +1,40 @@
+import {useState} from "react";
 
 import ProductCard from "../ProductCard";
-const products = [];
+const products = [
+  {
+    id:1,
+    title: "Apple",
+    price: "100000",
+  },
+  {
+    id:2,
+    title: "Samsung",
+    price: "30000",
+  },
+  {
+    id:3,
+    title: "Moto",
+    price: "115000",
+  }
+];
 
-function GetApiCall()
+function GetProductsApi(callback)
 {
   setTimeout(() => {
-    return products;
-  }, 1000);
+    callback(products);
+  }, 1000); //mock delay of api which is 1000 mili second.
 }
 
 //Api call to get the data 
 export default function Products() {
-  let gp=GetApiCall();
+  let gp=[];
+ GetProductsApi(
+     function(resp)
+     {
+     gp=resp;
+     }
+  );
 
   return (
     <div>
@@ -21,4 +44,65 @@ export default function Products() {
     </div>
   );
 }
+
+
+
+//frontend -> api -> backend
+// backend collects data and sends response to the frontend
+
+// state variable in react
+// I am a special kind of varibale
+// whose value once set would cause a rerender of the component
+// it would persist the value of the var on every rerender
+
+// Mounting --> first time process go through like :- index.js => app.js => products.js => productCard.js
+// rendering
+// index.js => app.js => products.js => productCard.js
+
+// hooks in react
+
+// hooks in react are nothing but a helper function
+// repetitive stuff in react you could delegate that to a function
+// functions job would be do the repetitive stuff
+// known as hooks in react
+
+// useState - system defined hook
+// it is always named like this
+// useName
+// useState(default Value)
+
+// return [stateVar, setterFn]
+// setteFn() { setting and rerendering}
+
+// Custom hooks
+// system defined hooks
+
+// hooks need to be at the top of the component
+// hooks can't be inside if conditions or for loops
+// hook can't be used in order to render a UI
+
+// understand how the child is getting rerendered
+// we will understand how to make this better
+// loader in our code
+
+// useEffect hook
+// useEffect(fn, []);
+// I will call this callback function
+// everytime dependent on this dependency array
+// when there is no dependency array
+// where there is an empty dependency array
+// when there is some value in this array
+// what can be passed in this dependency array
+// you can pass a state variable and you can pass a props from the parent which is a state variable
+
+// AJAX
+// fetch - ajax call with promises in order to get the data
+// axios
+
+// the concept which we would want to learn the barebone structure of fetch
+// is AJAX
+
+// Asyn js and xml
+// client => ajax => server => api => returns data => client => renders
+
 
